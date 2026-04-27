84dd402469dff9da657d76d593c4b281c3afe741
DURAND Malo <durand.malo.60590@gmail.com>
2026-04-27 17:11:25 -0400

    feat(backend): setup OpenAPI NestJS module (#1)

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('React Learning')
+    .setDescription('The Backend API for my React learning journey')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('openapi', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
-void bootstrap();
+bootstrap().catch(console.error);
