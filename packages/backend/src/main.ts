8f46ccc4ad9b969217b02ff07caa05a412484158
DURAND Malo <durand.malo.60590@gmail.com>
2026-04-27 19:53:23 -0400

    feat(backend): creating the health check route

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { cleanupOpenApiDoc, ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,9 +10,17 @@ async function bootstrap() {
     .setTitle('React Learning')
     .setDescription('The Backend API for my React learning journey')
     .setVersion('1.0')
+    .addTag('Health Check', 'All the health-check-related endpoints')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  const documentFactory = () =>
+    cleanupOpenApiDoc(SwaggerModule.createDocument(app, config));
   SwaggerModule.setup('openapi', app, documentFactory);
+
+  app.setGlobalPrefix('api', {
+    exclude: ['openapi', ''],
+  });
+
+  app.useGlobalPipes(new ZodValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
 }
