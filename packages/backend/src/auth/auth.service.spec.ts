9a6fac49b35a33fa4eb3488bc0f571268a231f83
DURAND Malo <durand.malo.60590@gmail.com>
2026-05-24 15:59:49 +0200

    fix(backend): jest and test environment

--- packages/backend/src/auth/auth.service.spec.ts
+++ packages/backend/src/auth/auth.service.spec.ts
@@ -1,4 +1,7 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service.js';
 import { AuthService } from './auth.service.js';
 
 describe('AuthService', () => {
@@ -6,7 +9,26 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: ConfigService,
+          useValue: {
+            getOrThrow: (key: string) => {
+              switch (key) {
+                case 'JWT_DURATION':
+                  return '1h';
+                case 'JWT_SECRET':
+                  return 'test-secret';
+                default:
+                  throw new Error(`No value found for ${key}`);
+              }
+            },
+          },
+        },
+        { provide: PrismaService, useValue: {} },
+        { provide: JwtService, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- packages/backend/src/oauth/oauth.service.spec.ts
+++ packages/backend/src/oauth/oauth.service.spec.ts
@@ -1,4 +1,7 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from '../auth/auth.service.js';
+import { PrismaService } from '../prisma/prisma.service.js';
 import { OauthService } from './oauth.service.js';
 
 describe('OauthService', () => {
@@ -6,7 +9,30 @@ describe('OauthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OauthService],
+      providers: [
+        OauthService,
+        {
+          provide: ConfigService,
+          useValue: {
+            getOrThrow: (key: string) => {
+              switch (key) {
+                case 'JWT_DURATION':
+                  return '1h';
+                case 'JWT_SECRET':
+                  return 'test-secret';
+                case 'GOOGLE_CLIENT_ID':
+                  return '<id>.apps.googleusercontent.com';
+                case 'GOOGLE_CLIENT_SECRET':
+                  return 'GOCSPX-<secret>';
+                default:
+                  throw new Error(`No value found for ${key}`);
+              }
+            },
+          },
+        },
+        { provide: PrismaService, useValue: {} },
+        { provide: AuthService, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<OauthService>(OauthService);
